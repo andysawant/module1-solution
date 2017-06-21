034693b635e96c414a7b67fc0c05ac6c5ef915c5
Sawant <andysawant@gmail.com>
2017-06-21 19:52:19 +1000

    assignment updated

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 (function(){
   'use strict';
-
   angular.module('myFirstApp',[])
-
-  .controller('MyFirstController',function($scope){
-
-    $scope.name="Aniruddha";
-    $scope.sayHello=function(){
-      return "Hello Coursera";
+  .controller('MyFirstController',MyFirstController);
+  MyFirstController.$inject=['$scope','$filter'];
+  function MyFirstController($scope,$filter){
+    $scope.foodItems="";
+    $scope.foodResponse="";
+    $scope.submitFoodItems=function(){
+      if(foodItemCount($scope.foodItems)==0){
+        $scope.foodResponse="Please enter data first";
+      }else if (foodItemCount($scope.foodItems)<=3){
+        $scope.foodResponse="Enjoy!";
+      }else {
+        $scope.foodResponse="Too much!";
+      }
     };
-  });
+  }
+  function foodItemCount(foodItems){
+    var count=0;
+    var foodItemArray=foodItems.split(",");
+    for(var i=0;i<foodItemArray.length;i++){
+      if(foodItemArray[i].trim().length!=0){
+        count++;
+      }
+    }
+    return count;
+  }
 })();
